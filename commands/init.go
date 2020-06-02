03a595e1267c377cb212315b8cc0cf4325c34bf7
arnaucube <root@arnaucube.com>
2020-06-02 20:20:00 +0200

    Add Tendermint init method to node

---
+++ commands/init.go
@@ -0,0 +1,89 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	cfg "github.com/tendermint/tendermint/config"
+
+	tmos "github.com/tendermint/tendermint/libs/os"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+	"github.com/tendermint/tendermint/p2p"
+	"github.com/tendermint/tendermint/privval"
+	"github.com/tendermint/tendermint/types"
+	tmtime "github.com/tendermint/tendermint/types/time"
+)
+
+func initTendermint(config *cfg.Config) error {
+	err := os.Mkdir("tmp", os.ModePerm)
+	if err != nil {
+		return err
+	}
+	err = os.Mkdir("tmp/config", os.ModePerm)
+	if err != nil {
+		return err
+	}
+	err = os.Mkdir("tmp/data", os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	configFile := "tmp/config/config.toml"
+	config.RootDir = filepath.Dir(filepath.Dir(configFile))
+
+	// private validator
+	privValKeyFile := config.PrivValidatorKeyFile()
+	privValStateFile := config.PrivValidatorStateFile()
+	var pv *privval.FilePV
+	if tmos.FileExists(privValKeyFile) {
+		pv = privval.LoadFilePV(privValKeyFile, privValStateFile)
+		logger.Info("Found private validator", "keyFile", privValKeyFile,
+			"stateFile", privValStateFile)
+	} else {
+		pv = privval.GenFilePV(privValKeyFile, privValStateFile)
+		pv.Save()
+		logger.Info("Generated private validator", "keyFile", privValKeyFile,
+			"stateFile", privValStateFile)
+	}
+
+	nodeKeyFile := config.NodeKeyFile()
+	if tmos.FileExists(nodeKeyFile) {
+		logger.Info("Found node key", "path", nodeKeyFile)
+	} else {
+		if _, err := p2p.LoadOrGenNodeKey(nodeKeyFile); err != nil {
+			return err
+		}
+		logger.Info("Generated node key", "path", nodeKeyFile)
+	}
+
+	// genesis file
+	genFile := config.GenesisFile()
+	if tmos.FileExists(genFile) {
+		logger.Info("Found genesis file", "path", genFile)
+	} else {
+		genDoc := types.GenesisDoc{
+			ChainID:         fmt.Sprintf("test-chain-%v", tmrand.Str(6)),
+			GenesisTime:     tmtime.Now(),
+			ConsensusParams: types.DefaultConsensusParams(),
+		}
+		pubKey, err := pv.GetPubKey()
+		if err != nil {
+			return fmt.Errorf("can't get pubkey: %w", err)
+		}
+		genDoc.Validators = []types.GenesisValidator{{
+			Address: pubKey.Address(),
+			PubKey:  pubKey,
+			Power:   10,
+		}}
+
+		if err := genDoc.SaveAs(genFile); err != nil {
+			return err
+		}
+		logger.Info("Generated genesis file", "path", genFile)
+	}
+
+	cfg.WriteConfigFile("tmp/config/config.toml", config)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module kvartalochain
+
+go 1.14
+
+require (
+	github.com/btcsuite/btcd v0.20.1-beta
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/dgraph-io/badger v1.6.1
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.6.3
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/viper v1.7.0
+	github.com/stretchr/testify v1.5.1
+	github.com/tendermint/tendermint v0.33.5
+	github.com/urfave/cli v1.22.4
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
+	gopkg.in/go-playground/assert.v1 v1.2.1
+)
